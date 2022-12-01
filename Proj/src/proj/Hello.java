4801003ea300a551e5b81769a17dbf2913c7139a
dalmo <mffacuri@terra.com.br>
2022-12-01 18:40:56 -0200

    Adicionando campo data de nascimento

--- Proj/src/proj/Hello.java
+++ Proj/src/proj/Hello.java
@@ -1,7 +1,11 @@
 package proj;
 
+import java.util.Date;
+
 public class Hello {
 
+	private Date dataNascimento;
+	
 	public static void main(String[] args) {
 		 System.out.println("Hello, world!!");
 
