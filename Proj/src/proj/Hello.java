0bc69137145d4a447e85978e030cecd1f181bf91
dalmo <mffacuri@terra.com.br>
2022-12-05 14:28:02 -0200

    alterações na branch

--- Proj/src/proj/Hello.java
+++ Proj/src/proj/Hello.java
@@ -6,6 +6,14 @@ public class Hello {
 
 	private Date dataNascimento;
 	
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
 	public static void main(String[] args) {
 		 System.out.println("Hello, world!!");
 
